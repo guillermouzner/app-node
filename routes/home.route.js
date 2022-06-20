fc5cda80f8fcd97210164a293c1621609996443f
guillermouzner <guillermouz16@gmail.com>
2022-06-20 19:19:28 -0300

    fix bugs

--- routes/home.route.js
+++ routes/home.route.js
@@ -10,10 +10,10 @@ import {
 import { validarUrl } from "../middlewares/urlValidator.js";
 import { verificarUser } from "../middlewares/userValidator.js";
 
-import {
-    formPerfil,
-    editarFotoPerfil,
-} from "../controllers/perfil.controller.js";
+// import {
+//     formPerfil,
+//     editarFotoPerfil,
+// } from "../controllers/perfil.controller.js";
 
 const router = Router();
 
@@ -24,8 +24,8 @@ router.get("/eliminar/:id", verificarUser, eliminarUrl);
 router.get("/editar/:id", verificarUser, editarUrl);
 router.post("/editar/:id", verificarUser, validarUrl, editarUrlForm);
 
-router.get("/perfil", verificarUser, formPerfil);
-router.post("/perfil", verificarUser, editarFotoPerfil);
+// router.get("/perfil", verificarUser, formPerfil);
+// router.post("/perfil", verificarUser, editarFotoPerfil);
 
 router.get("/:shortUrl", redireccion);
 export default router;
